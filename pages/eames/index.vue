755e022e3c74ea0f17579c492acabb4ad8f46070
Morgan Koh <delacrixmorgan@pm.me>
2021-07-12 23:42:03 +0800

    Import Google Model-Viewer

---
+++ pages/eames/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <model-viewer
+      id='viewer'
+      class='w-full h-screen'
+      src='@/public/assets/Astronaut.glb'
+      autoplay camera-controls field-of-view='45deg'
+      interaction-prompt='none'
+      ar ar-modes='webxr scene-viewer quick-look'></model-viewer>
+  </div>
+</template>
+
+<script>
+import('@google/model-viewer')
+export default {}
+</script>
