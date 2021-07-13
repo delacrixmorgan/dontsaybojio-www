27fa6fcca9906ccd18f5256c258c5b0cb5a8da19
Morgan Koh <delacrixmorgan@pm.me>
2021-07-13 23:41:22 +0800

    Able to display model-viewer with static objects

--- pages/eames/index.vue
+++ pages/eames/index.vue
@@ -3,14 +3,36 @@
     <model-viewer
       id='viewer'
       class='w-full h-screen'
-      src='@/public/assets/Astronaut.glb'
-      autoplay camera-controls field-of-view='45deg'
+      src='/sheen_chair.glb'
+      auto-rotate camera-controls field-of-view='45deg'
       interaction-prompt='none'
-      ar ar-modes='webxr scene-viewer quick-look'></model-viewer>
+      environment-image='neutral'
+      option='Peacock Velvet'
+      ar ar-modes='webxr scene-viewer quick-look' />
   </div>
 </template>
 
 <script>
 import('@google/model-viewer')
-export default {}
+
+export default {
+  created() {
+    // const modelViewerVariants = this.document.querySelector('model-viewer#viewer')
+    // const select = this.document.querySelector('#variant')
+    //
+    // modelViewerVariants.addEventListener('load', () => {
+    //   const names = modelViewerVariants.availableVariants
+    //   for (const name of names) {
+    //     const option = this.document.createElement('option')
+    //     option.value = name
+    //     option.textContent = name
+    //     select.appendChild(option)
+    //   }
+    // })
+    //
+    // select.addEventListener('input', (event) => {
+    //   modelViewerVariants.variantName = event.target.value
+    // })
+  }
+}
 </script>
